cbb8b27ed0a51058b7b3d7243f2cf0922747345f
Chase Carnaroli <chasecarnaroli@gmail.com>
2025-03-18 01:03:36 -0700

    Implement download button navigation to app stores based on device type
    No longer goes to /download first. Imo, this is a better user experience.

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ function toggleNavbar() {
         navbar.classList.add('show');
     }
 }
+
+/**
+ * Navigates to the appropriate app store based on the user's device
+ * If the user is on an Android device, it opens the Google Play Store link
+ * Otherwise, it opens the Apple App Store link
+ * The App Store is the default because the listing is nicer
+ */
+function navigateToDownload() {
+    const isAndroid = /Android/.test(navigator.userAgent);
+
+    if (isAndroid) {
+        // Open the Google Play Store link
+        window.open("https://play.google.com/store/apps/details?id=com.chasecarnaroli.qrme_contact", "_blank");
+    } else {
+        // Open the Apple App Store link
+        // This is also the default if the user is coming from qrme.contact because the App Store listing is nicer
+        window.open("https://apps.apple.com/app/apple-store/id1412627381?pt=119166549&ct=QR%20Me%20Website&mt=8&platform=iphone", "_blank");
+    }
+}
