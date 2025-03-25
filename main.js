b9faed1c666093af52eb4bf7f1032f9ecb7bd956
Chase Carnaroli <chasecarnaroli@gmail.com>
2025-03-24 23:28:38 -0700

    Prefill the Contact form's device data

--- main.js
+++ main.js
@@ -28,3 +28,33 @@ function navigateToDownload() {
         window.open("https://apps.apple.com/app/apple-store/id1412627381?pt=119166549&ct=QR%20Me%20Website&mt=8&platform=iphone", "_blank");
     }
 }
+
+/**
+ * Generates a Google Forms link with the query parameters from the URL
+ * The query parameters are mapped to the corresponding Google Forms fields
+ * The generated link is used to pre-fill the form with the user's information
+ *
+ * @returns {string} The generated Google Forms link
+ */
+function generateGoogleFormsLink() {
+    // Map the query parameters to the Google Forms fields
+    const googleFormFields = {
+        platform: 'entry.1498958727',
+        device: 'entry.1473862599',
+        osVersion: 'entry.388596581',
+        qrmeVersion: 'entry.571404993'
+    };
+
+    // Get the query parameters from the URL
+    const params = new URLSearchParams(window.location.search);
+
+    const platform = params.get('platform') || '';
+    const device = params.get('device') || '';
+    const osVersion = params.get('os_version') || '';
+    const qrmeVersion = params.get('qrme_version') || '';
+
+    // Construct the Google Forms link with the query parameters
+    const googleFormsLink = `https://docs.google.com/forms/d/e/1FAIpQLScqT77ZMt-Avzk8JQsEDO4SbwZD4e6jxcIQiJmWt58ZdUiYdA/viewform?usp=pp_url&${googleFormFields.platform}=${platform}&${googleFormFields.device}=${device}&${googleFormFields.osVersion}=${osVersion}&${googleFormFields.qrmeVersion}=${qrmeVersion}&embedded=true`;
+
+    return googleFormsLink;
+}
