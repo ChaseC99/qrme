4c266c700ff26804e7f6841dd8fbb8a9af03c53f
Chase Carnaroli <chasecarnaroli@gmail.com>
2025-09-04 18:13:06 -0700

    Add event tracking for link clicks

--- src/components/DynamicDownloadButton.tsx
+++ src/components/DynamicDownloadButton.tsx
@@ -7,9 +7,18 @@ export default function DownloadButton() {
     const url = isAndroid
         ? "https://play.google.com/store/apps/details?id=com.chasecarnaroli.qrme_contact&utm_source=website"
         : "https://apps.apple.com/app/apple-store/id1412627381?pt=119166549&ct=QR%20Me%20Website&mt=8&platform=iphone";
-    
+
+    const ariaLabel = isAndroid ? "Download on Google Play" : "Download on App Store";
+
     return (
-        <a href={url} style={styles.downloadButton}>Download</a>
+        <a 
+            href={url} 
+            style={styles.downloadButton} 
+            aria-label={ariaLabel}
+            target="_blank"
+        >
+            Download
+        </a>
     );
 }
 
